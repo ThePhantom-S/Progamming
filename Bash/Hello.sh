d799a440f50ad3ec6d35f51f4dbf375250c592eb
Harikrishnan <rharikrishnan8726@gmail.com>
2026-03-27 10:22:57 +0530

    printing Script committing

---
+++ Bash/Hello.sh
@@ -0,0 +1,7 @@
+echo "--------------------"		#echo used for output
+echo "||||Hello World!||||"
+echo "--------------------"
+
+echo "Enter your Name : "
+read name				#read used for getting input from user
+echo "Hi $name Hope you are doing well."
